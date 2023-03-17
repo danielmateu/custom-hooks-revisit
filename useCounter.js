1aa6587c13e3eb0296468a8dcfa5b1c261e6dd90
Daniel Mateu <danielmateu86@gmail.com>
2023-03-17 15:41:11 +0100

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useCounter = (initialState = 1) => {
+
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = (value) => {
+        setCounter((counter) => counter + value);
+    }
+
+    const decrement = (value) => {
+        if (counter <= 0) return
+        setCounter((counter) => counter - value);
+    }
+
+    const reset = () => {
+        setCounter(initialState);
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    };
+}
